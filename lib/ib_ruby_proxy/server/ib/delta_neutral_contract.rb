e1dd4213c531737e75da27824614c1e3b0658a52
Nick Pellant <nick@nickpellant.com>
2019-10-26 19:06:43 +0100

    Regenerate Ruby classes

--- lib/ib_ruby_proxy/server/ib/delta_neutral_contract.rb
+++ lib/ib_ruby_proxy/server/ib/delta_neutral_contract.rb
@@ -2,14 +2,14 @@
 # File generated automatically by ib_ruby_proxy
 # ---------------------------------------------
 
-java_import "com.ib.client.DeltaNeutralContract"
+java_import 'com.ib.client.DeltaNeutralContract'
 
 class Java::ComIbClient::DeltaNeutralContract
   def to_ruby
     ruby_object = IbRubyProxy::Client::Ib::DeltaNeutralContract.new
-    ruby_object.conid = conid().to_ruby
-    ruby_object.delta = delta().to_ruby
-    ruby_object.price = price().to_ruby
+    ruby_object.conid = conid.to_ruby
+    ruby_object.delta = delta.to_ruby
+    ruby_object.price = price.to_ruby
 
     ruby_object
   end
